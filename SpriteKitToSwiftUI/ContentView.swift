1cacee3c5fd56ce853b13d605b3f693c2c4a9ff5
timothy <tandrian24@bsd.idserve.net>
2024-06-30 23:11:23 +0700

    Initial Commit

---
+++ SpriteKitToSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpriteKitToSwiftUI
+//
+//  Created by Timothy Andrian on 30/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpriteKitToSwiftUI/SpriteKitToSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitToSwiftUIApp.swift
+//  SpriteKitToSwiftUI
+//
+//  Created by Timothy Andrian on 30/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitToSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
